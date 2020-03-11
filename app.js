8cf27a70f1ca82d173b4f9c8078a2023ec2b820d
TomRott <tom.rottenfus@gmail.com>
2020-03-11 15:56:32 +0000

    message

---
+++ app.js
@@ -0,0 +1,18 @@
+firstName="Tom";
+favouriteColor="Orange";
+favouriteFood="Chicken";
+city = "Pontoise";
+//Write firstName, favouriteColor and FavouriteFood
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood);
+phrase = "A rainbow is formed by the sunlight. Shining through rain drops sosmall. The rain bends the light from the sunshine, and colors the sky for us all.";
+endingPhrase ="is what we see. Rainbow colours, are made with light bendingyou see!";
+one = "Red";
+two = "orange";
+three = "yellow";
+four = "green";
+five = "blue";
+six = "indigo";
+seven = "violet";
+console.log(phrase + one +", " + two +", " + three +", "+  four + ", "+five + ", "+ six +", " + seven +", "+ endingPhrase)
